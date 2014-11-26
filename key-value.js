d0d298721fbe495379820bf695d2f7d2ae07a0d8
Mark Macdonald <mark.t.macdonald@googlemail.com>
2014-11-26 10:36:01 +0100

    initial solution for key-value question.

---
+++ key-value.js
@@ -0,0 +1,37 @@
+
+/*
+ The Foo class implements private properties by Douglas Crockford's method
+*/
+
+function Foo() {
+
+	//values is a private property
+	var values = {};
+
+	//set() is public method
+	this.set = function (key, value) {
+		values[key] = value;
+	};
+
+	//get() is a public method
+	this.get = function (key) {
+		return values[key];
+	};
+
+};
+
+var instance = new Foo();
+
+var instance2 = new Foo();
+
+instance.set('key', 'monkeys');
+
+console.log(instance.get('key')); // returns value
+
+console.log(instance.key); // Error (undefined)
+
+instance2.set('key', 'boomerangs');
+
+console.log(instance2.get('key')); //returns value2
+
+console.log(instance2.key); // Error (undefined)
